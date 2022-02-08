818ed70be430d013855c2f9bc0449705588229a3
Soralei <62912193+Soralei@users.noreply.github.com>
2022-02-08 19:37:23 +0100

    na

--- auth.js
+++ auth.js
@@ -70,7 +70,7 @@ function UserExists(username){
             }
         });
     
-        resolve(true); // No user exists.
+        resolve(false); // No user exists.
     });
 }
 
@@ -87,9 +87,7 @@ async function RegisterUser(username, password, email, callback){
         return callback({error: `Failed to register user. Email failed to validate.`});
     }
 
-    const userExists = await UserExists(username);
-    console.log(userExists);
-    if(userExists){
+    if(await UserExists(username)){
         return callback({error: `Failed to register user. Username is already registered.`});
     }
 
