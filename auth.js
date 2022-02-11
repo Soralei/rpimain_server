b0bf3da2fa10844ef8152bb071914817a3479c5c
Soralei <62912193+Soralei@users.noreply.github.com>
2022-02-11 21:34:23 +0100

    test

--- auth.js
+++ auth.js
@@ -120,6 +120,8 @@ function UserExists(username){
 
 function VerifyPassword(input_password, db_password, salt_secret, salt_rounds, callback){
     console.log(db_password);
+    console.log(`salt_secret ${salt_secret}`);
+    console.log(`salt_rounds ${salt_rounds}`);
     const scrambled_input = ScramblePassword(input_password, {base_secret: process.env.PW_SECRET, salt_secret: salt_secret, salt_rounds: salt_rounds});
     console.log(scrambled_input);
     if(scrambled_input == db_password){
